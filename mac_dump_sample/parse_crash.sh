2b0a4a83a23cecb67adfb17569edf893760ed3e0
peterzjzhu <peterzjzhu@tencent.com>
2023-04-14 14:39:34 +0800

    parse_crash.sh支持传参数 arm64 ./parse_crash.sh arm64

--- mac_dump_sample/parse_crash.sh
+++ mac_dump_sample/parse_crash.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "input args num $#"
+echo "input args num $1"
+APP_ARCH="x86_64"
+if [ $# -gt 0 ]; then
+    if [ $1 = "arm64" ]; then
+      APP_ARCH="arm64"
+    fi
+fi
+echo "APP_ARCH: ${APP_ARCH}"
+
+
 function checkFileIfNotExit(){
 	if [ ! -e $1 ];then
 		echo $1"不存在，请确认该文件存在后再试！"
@@ -6,7 +17,7 @@ function checkFileIfNotExit(){
 	fi
 }
 #替换成你自己的应用名字
-APP_NAME="TMSDKFramework"
+APP_NAME="tmsdk_wemeet_sdk"
 APP_DYSM_FILE=$APP_NAME".framework.dSYM"
 APP_SYM_FILE=$APP_NAME".sym"
 
@@ -24,7 +35,13 @@ checkFileIfNotExit $CMD_DUMP_SYM_FILE
 checkFileIfNotExit $CMD_DUMP_STACK_FILE
 
 echo “/*********** dSYM文件转sym文件开始执行，可能需要等待几十秒到几分钟..... ************/”
-trans=`$CMD_DUMP_SYM_FILE $APP_DYSM_FILE > $APP_SYM_FILE`
+TRANS_ARCH=""
+if [ ${APP_ARCH} = "arm64" ]; then
+    TRANS_ARCH="-a arm64"
+fi
+echo "TRANS_ARCH: ${TRANS_ARCH}"
+trans=`$CMD_DUMP_SYM_FILE $TRANS_ARCH $APP_DYSM_FILE > $APP_SYM_FILE`
+#trans=`$CMD_DUMP_SYM_FILE -a arm64 $APP_DYSM_FILE > $APP_SYM_FILE`
 #./dump_syms -a arm64 TMSDKFramework.framework.dSYM > TMSDKFrameworkArm64.sym
 # echo "result=$?"
 result=$?
